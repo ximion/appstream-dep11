357469b28430a9aa3c7420f0a7dc5399bb9cc585
Matthias Klumpp <matthias@tenstral.net>
2016-04-12 15:57:57 +0200

    trivial: post release version bump

--- dep11/__init__.py
+++ dep11/__init__.py
@@ -4,4 +4,4 @@ from .extractor import MetadataExtractor
 from .component import Component, Screenshot, IconSize, DEP11YamlDumper, ProvidedItemType, IconType
 from .datacache import DataCache
 
-__version__ = '0.5'
+__version__ = '0.6'
